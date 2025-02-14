50151ec779249a3f49b928b79b07b2a274aac672
Mrikh-AS <mrih@mail.ru>
2025-02-14 15:01:01 +0300

    1

--- main.py
+++ main.py
@@ -24,20 +24,22 @@ class ContractAssessment:
         self.input = None
         self.messages = []
         self.rights = None
+        self.analysis = None
         self.report = None
 
     def read(self):
+        file_path = Path(__file__).parent / "data" / "raw" / "1.txt"
         with open(file_path, mode="r", encoding="utf-8") as file:
             self.input = file.read()
 
     def evaluation(self):
         logger.info("Проверяю, является ли текст, введенный пользователем,договором")
-        logger.debug(f"Текст: {self.input}")
+        # logger.debug(f"Текст: {self.input}")
         self.messages.append(
             [
                 {
                     "role": "system",
-                    "content": """Ты - юрист. Оцени по шкале от 0 до 100 процентов, с какой вероятностью
+                    "content": """Ты - junior-юрист. Оцени по шкале от 0 до 100 процентов, с какой вероятностью
                     введенный пользователем текст является договором.
                     Ответ дай в формате json. Пример ответа:
                     {
@@ -46,15 +48,15 @@ class ContractAssessment:
                                 """,
                 },
                 {"role": "user", "content": self.input},
-            ],
+            ]
         )
         response = client.chat.completions.create(
             model="deepseek-chat",
             messages=self.messages,
             response_format={"type": "json_object"},
         )
-        full_message = response.choices[0].message
-        self.messages.append(full_message)
+        # full_message = response.choices[0].message
+        # self.messages.append(full_message)
         result = json.loads(response.choices[0].message.content)
         logger.info(f"Проверка завершена. Вероятность: {result['Probability']}")
         if result["Probability"] < 80:
@@ -101,40 +103,70 @@ class ContractAssessment:
         logger.info(f"Права и обязанности извлечены:\n{self.rights}")
         return self.rights
 
-    def rule_recall(self):
-        return result
+    # def rule_recall(self):
+    #    return result
 
     def rule_application(self):
-        return result
+        logger.info(
+            "Проверяю извлеченные права и обязанности на предмет соовтетствия закону, формирую рекомендации."
+        )
+        self.messages.append(
+            {
+                "role": "system",
+                "content": f"""Ты - middle-юрист.
+                Ты хорошо умеешь находить релевантную норму ГК РФ к положению договора, сравнивать положение договора с релевантной нормой права 
+                и делать вывод по результатам такого сопоставления.
+                Ранее специалист в области обработки естественного языка извлек для тебя список простых предложений из текста договора:{self.rights} 
+                Эти предложения являются правами и обязанностями сторон договора. Найди под каждое предложение релевантную норму права и сделай вывод,
+                не противоречит ли право или обязанность из договора релевантной норме права. Если противоречие есть - дай рекомендацию.
+                Если релевантная норма ГК РФ отсутствует - не выдумывай, прямо скажи об этом.
+                Ответ дай на естественном языке.
+                            """,
+            }
+            # "role": "user",
+            # "content": "хмм"
+        )
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=self.messages,
+            response_format={"type": "json_object"},
+        )
+        full_message = response.choices[0].message
+        self.messages.append(full_message)
+        self.rights = json.loads(response.choices[0].message.content)
+        logger.info(f"Права и обязанности извлечены:\n{self.rights}")
+        return self.rights
+        return self.analysis
 
     def report(self):
         logger.info("Генерирую итоговый доклад.")
         self.messages.append(
-            [
-                {
-                    "role": "system",
-                    "content": """Ты - юрист. Оцени по шкале от 0 до 100 процентов, с какой вероятностью
-                    введенный пользователем текст является договором.
-                    Ответ дай в формате json. Пример ответа:
-                    {
-                                "Probability": "80"
-                    }
-                                """,
-                },
-                {"role": "user", "content": self.input},
-            ],
+            # [
+            {
+                "role": "system",
+                "content": f"""Ты - senior-юрист, руководитель команды. Твоя задача - кратко рассказать клиенту о проделанной работе
+                    и дать клиенту рекомендации, основанные на выводах middle-юриста: {self.analysis}
+                    Ответ дай в форме заключения юридической службы на естественном языке, излагающее суть проделанной работы и список рекомендаций.
+                    """,
+            },
+            # {"role": "user", "content": self.input},
+            # ],
         )
-        self.report = "Здесь будет итоговый результат проверки."
-        return result
+        response = client.chat.completions.create(
+            model="deepseek-chat",
+            messages=self.messages,
+        )
+        self.report = json.loads(response.choices[0].message.content)
+        logger.info("Заключение подготовлено.")
+        print(f"self.report")
 
     def process_all(self):
         result = self.evaluation()
         if result:
             self.contract_spotting()
             # self.rule_recall()
-            # self.rule_application()
+            self.rule_application()
             self.report()
-            print(f"{self.report}")
         else:
             print("Проверка остановлена. Загрузите текст договора.")
 
@@ -145,4 +177,8 @@ class ContractAssessment:
 file_path = Path(__file__).parent.parent / "data" / "raw" / "1.txt"
 
 example = ContractAssessment()
-example.process_all()
+
+example.read()
+print(example.input)
+type(example.input)
+example.evaluation()
